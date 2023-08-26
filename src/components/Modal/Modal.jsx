d97fd7294a32970dfd899e1ce1512a666de14c3c
Iryna220 <ibiriszka951@gmail.com>
2023-08-26 20:59:39 +0200

    update modal hook

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,27 +2,32 @@ import styles from './Modal.module.css';
 import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
-const Modal = ({ toggleModal, children }) => {
-  const handleKeydown = evt => {
-    if (evt.code === 'Escape') toggleModal();
-  };
-  const handleBackdropClick = evt => {
-    if (evt.currentTarget === evt.target) {
-      toggleModal();
-    }
-  };
+const modalRoot = document.querySelector('#modal-root');
+const Modal = ({ toggleModal, largeImage }) => {
   useEffect(() => {
+    const handleKeydown = evt => {
+      if (evt.code === 'Escape') toggleModal();
+    };
     window.addEventListener('keydown', handleKeydown);
     return () => {
       window.removeEventListener('keydown', handleKeydown);
     };
   }, [toggleModal]);
-  const modalRoot = document.querySelector('#modal-root');
+
+  const handleBackdropClick = evt => {
+    if (evt.currentTarget === evt.target) {
+      toggleModal();
+    }
+  };
+
   return createPortal(
     <div className={styles.Overlay} onClick={handleBackdropClick}>
-      <div className={styles.Modal}>{children}</div>
+      <div className={styles.Modal}>
+        <img src={largeImage.src} alt="" />
+      </div>
     </div>,
     modalRoot
   );
 };
+
 export default Modal;
